535af0418936310b01ee5230255c67c67c2577ca
Illia Bodnar <illia.v.bodnar@gmail.com>
2024-10-14 19:59:03 +0300

    lol

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -5,11 +5,73 @@ import { Text } from "react-native-paper";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { SoundCard } from "../components/";
 import { SOUNDS } from "../constants";
+import React from "react";
+import * as Notifications from "expo-notifications";
+
+import { Platform } from "react-native";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: false,
+  }),
+});
+
+// Function to register for push notifications
+async function registerForPushNotificationsAsync() {
+  let token;
+
+  await Notifications.setNotificationChannelAsync("alert", {
+    name: "alert",
+    importance: Notifications.AndroidImportance.HIGH,
+    sound: "atomne.wav", // Provide ONLY the base filename
+  });
+
+  // Get the push token
+  const { status: existingStatus } = await Notifications.getPermissionsAsync();
+  let finalStatus = existingStatus;
+  if (existingStatus !== "granted") {
+    const { status } = await Notifications.requestPermissionsAsync();
+    finalStatus = status;
+  }
+  if (finalStatus !== "granted") {
+    alert("Failed to get push token for push notification!");
+    return;
+  }
+
+  token = (await Notifications.getExpoPushTokenAsync()).data;
+  console.log(token);
+
+  return token;
+}
 
 export default function Home() {
   const [isAirRaidAlert, setIsAirRaidAlert] = useState(false);
   const [selectedSound, setSelectedSound] = useState(0);
   const insets = useSafeAreaInsets();
+
+  React.useEffect(() => {
+    // Request permissions for notifications
+    registerForPushNotificationsAsync();
+
+    // Listen for incoming notifications
+    const notificationListener = Notifications.addNotificationReceivedListener(
+      (notification) => {
+        console.log("Notification received:", notification);
+      }
+    );
+
+    const responseListener =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log("Notification response:", response);
+      });
+
+    return () => {
+      Notifications.removeNotificationSubscription(notificationListener);
+      Notifications.removeNotificationSubscription(responseListener);
+    };
+  }, []);
 
   const sounds = SOUNDS.map((sound, index) => {
     const handleElementPress = () => {
